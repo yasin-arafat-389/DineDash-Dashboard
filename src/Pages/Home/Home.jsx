3b5d32731eb1a4933765ba39f559dda26c9690d0
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2023-12-31 11:37:13 +0600

    Initialized and configured folder structure

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <div>This is homepage</div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
